c24241d1a35bc38e6e547f61f623ec4965585d5c
EuphCat <EuphoricCatface@gmail.com>
2020-12-06 20:37:10 +0900

    Grouping into classes: StrIndex, Paddable

--- altar_string_mod.py
+++ altar_string_mod.py
@@ -10,9 +10,9 @@ import os
 WORK_FOLDER="./UTKRStrMod"
 INDEX_CACHE_FILE_NAME="str_cache.txt"
 JSON_FILE_NAME="strings.json"
-FILE_CONTENTS = None
 
-PADDING_FILTERED = set()
+INDEX_FILE = None
+ASM_FILE = None
 
 def print_help():
     print("==== 도움말 ====")
@@ -31,18 +31,16 @@ def print_help():
     print("==== ====== ====")
 
 def input_cmd_parser(input_str):
-    global FILE_CONTENTS
-    global PADDING_FILTERED
     cmd = input_str[0]
     if cmd == "h":
         print_help()
     elif cmd == "p":
-        [FILE_CONTENTS, PADDING_FILTERED] = hangul_padding_toggle(FILE_CONTENTS, PADDING_FILTERED)
+        INDEX_FILE.hangul_padding_toggle()
     elif cmd == "s":
         search()
     elif cmd == "i":
-        create_index_cache()
-        open_index_cache()
+        INDEX_FILE.create_index_cache()
+        INDEX_FILE.open_index_cache()
     elif cmd == "q":
         sys.exit()
     else:
@@ -69,110 +67,184 @@ def input_decimal_parser(input_str):
         return range(start, end)
     return [int(input_str)]
 
-def hangul_padding_toggle(file_contents, padding_filtered):
-    if len(padding_filtered) == 0:
-        print("공백 필터링 적용 중...")
-        for i in range(len(file_contents)):
-            unpadded = hangul_pad_del(file_contents[i])
-            if unpadded is None:
+
+class Paddable:
+    def __init__(self):
+        self.padding_filtered = set()
+
+    def hangul_padding_toggle(self):
+        if len(self.padding_filtered) == 0:
+            print("공백 필터링 적용 중...")
+            for i in range(len(self.file_contents)):
+                unpadded = self.hangul_pad_del(self.file_contents[i])
+                if unpadded is None:
+                    continue
+                self.file_contents[i] = unpadded
+                self.padding_filtered.add(i)
+        else:
+            print("공백 필터링 해제 중...")
+            padded_list = list(self.padding_filtered)
+            padded_list.sort()
+            for i in padded_list:
+                padded = self.hangul_pad_add(self.file_contents[i])
+                self.file_contents[i] = padded
+                self.padding_filtered.remove(i)
+
+    @staticmethod
+    def hangul_pad_del(input_line):
+        # returns None if the line doesn't seem to be padded
+
+        # Let's try preserving all trailing spaces
+        rstrip_len = len(input_line.rstrip())
+        trail_sp = input_line[rstrip_len:]
+
+        # testing if the string is padded - assuming every "Lo" is hangul
+        # 1. A row has to have at least one "Lo"
+        # 2. Every "Lo" has to be followed by space
+        has_lo = False
+        lo_but_no_sp = False
+        no_pad_buf = str()
+
+        skip_flag = False
+
+        for j in range(len(input_line)):
+            if skip_flag:
+                skip_flag = False
                 continue
-            file_contents[i] = unpadded
-            padding_filtered.add(i)
-    else:
-        print("공백 필터링 해제 중...")
-        padded_list = list(padding_filtered)
-        padded_list.sort()
-        for i in padded_list:
-            padded = hangul_pad_add(file_contents[i])
-            file_contents[i] = padded
-            padding_filtered.remove(i)
+            new_chr = input_line[j]
+            no_pad_buf += new_chr
 
-    return [file_contents, padding_filtered]
-
-def hangul_pad_del(input_line):
-    # returns None if the line doesn't seem to be padded
-
-    # Let's try preserving all trailing spaces
-    rstrip_len = len(input_line.rstrip())
-    trail_sp = input_line[rstrip_len:]
-
-    # testing if the string is padded - assuming every "Lo" is hangul
-    # 1. A row has to have at least one "Lo"
-    # 2. Every "Lo" has to be followed by space
-    has_lo = False
-    lo_but_no_sp = False
-    no_pad_buf = str()
-
-    skip_flag = False
-
-    for j in range(len(input_line)):
-        if skip_flag:
-            skip_flag = False
-            continue
-        new_chr = input_line[j]
-        no_pad_buf += new_chr
-
-        if unicodedata.category(new_chr) != "Lo":
-            continue
-
-        has_lo = True
-
-        if j+1 == len(input_line):
-            # lo_but_no_sp = True
-            # we have already deleted all trailing space
-            break
-
-        next_chr = input_line[j+1]
-        if next_chr != " ":
-            # & is linebreak, and appears right after "Lo" in padded
-            # We have to preserve this, while not messing up paddedness detection
-            if next_chr == "&":
+            if unicodedata.category(new_chr) != "Lo":
                 continue
-            lo_but_no_sp = True
-            break
 
-        # Current chr is Lo, and next is (padding) space.
-        # We don't need to check the next, and also skip adding no_pad_buf
-        skip_flag = True
+            has_lo = True
 
-    if not has_lo:
-        return None
-    if lo_but_no_sp:
-        return None
+            if j+1 == len(input_line):
+                # lo_but_no_sp = True
+                # we have already deleted all trailing space
+                break
 
-    return no_pad_buf + trail_sp
+            next_chr = input_line[j+1]
+            if next_chr != " ":
+                # & is linebreak, and appears right after "Lo" in padded
+                # We have to preserve this, while not messing up paddedness detection
+                if next_chr == "&":
+                    continue
+                lo_but_no_sp = True
+                break
 
-def hangul_pad_add(input_line):
-    # Let's try preserving all trailing spaces
-    rstrip_len = len(input_line.rstrip())
-    trail_sp = input_line[rstrip_len:]
+            # Current chr is Lo, and next is (padding) space.
+            # We don't need to check the next, and also skip adding no_pad_buf
+            skip_flag = True
 
-    padded_buf = str()
+        if not has_lo:
+            return None
+        if lo_but_no_sp:
+            return None
 
-    for j in range(len(input_line)):
-        new_chr = input_line[j]
-        padded_buf += new_chr
-        if unicodedata.category(new_chr) != "Lo":
-            continue
+        return no_pad_buf + trail_sp
 
-        if j+1 == len(input_line):
-            break
+    @staticmethod
+    def hangul_pad_add(input_line):
+        # Let's try preserving all trailing spaces
+        rstrip_len = len(input_line.rstrip())
+        trail_sp = input_line[rstrip_len:]
 
-        # No padding before linebreak(&)
-        if input_line[j+1] == "&":
-            continue
+        padded_buf = str()
 
-        # Now probably good to pad
-        padded_buf += " "
+        for j in range(len(input_line)):
+            new_chr = input_line[j]
+            padded_buf += new_chr
+            if unicodedata.category(new_chr) != "Lo":
+                continue
 
-    return padded_buf + trail_sp
+            if j+1 == len(input_line):
+                break
 
-def print_line(linenum):
-    line_content = FILE_CONTENTS[linenum]
-    if len(PADDING_FILTERED) == 0:
+            # No padding before linebreak(&)
+            if input_line[j+1] == "&":
+                continue
+
+            # Now probably good to pad
+            padded_buf += " "
+
+        return padded_buf + trail_sp
+
+class StrIndex(Paddable):
+    def __init__(self, path):
+        Paddable.__init__(self)
+        self.cache_path = path
+        self.file_contents = None
+
+        if not self.open_index_cache():
+            print("Index cache was not found")
+            print("Opening {} instead".format(JSON_FILE_NAME))
+            if not os.path.isfile(JSON_FILE_NAME):
+                print("FATAL: {} was not found too".format(JSON_FILE_NAME))
+                sys.exit(-1)
+            with open(JSON_FILE_NAME) as input_file:
+                # input_file_contents = input_file.read()
+                print("{} was successfully open".format(JSON_FILE_NAME))
+                self.file_contents = json.load(input_file)
+                print("json successfully parsed")
+
+    def open_index_cache(self):
+        if not os.path.isfile(self.cache_path):
+            return False
+        with open('{}/{}'.format(WORK_FOLDER, INDEX_CACHE_FILE_NAME)) as idx_cache_file:
+            idx_cache_list = idx_cache_file.readlines()
+        print("Index cache was successfully open")
+        self.file_contents = self.DeserializedLines(idx_cache_list)
+        print("Deserialization complete")
+        return True
+
+    def create_index_cache(self):
+        if not os.path.isdir("./code"):
+            print("ERROR: code folder was not found")
+            return False
+
+        # subprocess is probably better but whatever ¯\_(ツ)_/¯
+        if not os.path.isdir(WORK_FOLDER):
+            os.mkdir(WORK_FOLDER)
+        print("Creating index cache...")
+        os.system('grep -r ./code/ -e "push\\.cst string" > {}'
+                .format(self.cache_path))
+        print("Index cache was created")
+        return True
+
+    class DeserializedLines:
+        class DeserializedLine:
+            def __init__(self, input_line):
+                input_line = input_line.strip()
+                splt_ln = input_line.split(":")
+                self.path = splt_ln[0]
+                self.asmaddr = splt_ln[1]
+
+                idx_quot = input_line.find('"')
+                self.line = input_line[idx_quot+1:-1]
+
+        def __init__(self, input_lines):
+            self.lnlist = []
+            for line in input_lines:
+                self.lnlist.append(self.DeserializedLine(line))
+            self.edited_lines = set()
+
+        def __getitem__(self, key):
+            return self.lnlist[key].line
+
+        def __setitem__(self, key, value):
+            self.lnlist[key].line = value
+
+        def __len__(self):
+            return len(self.lnlist)
+
+
+def print_line(prn_file, linenum):
+    line_content = prn_file.file_contents[linenum]
+    if len(prn_file.padding_filtered) == 0:
         print(linenum, line_content)
     else:
-        flag = linenum in PADDING_FILTERED
+        flag = linenum in prn_file.padding_filtered
         if flag:
             print("[*]", linenum, line_content)
         else:
@@ -183,9 +255,9 @@ def search():
     print(">> ", end="", flush=True)
     input_str = stdin.readline()
     input_str = input_str[:-1] # assuming \n
-    for i in range(len(FILE_CONTENTS)):
-        if input_str in FILE_CONTENTS[i]:
-            print_line(i)
+    for i in range(len(INDEX_FILE.file_contents)):
+        if input_str in INDEX_FILE.file_contents[i]:
+            print_line(INDEX_FILE, i)
 
 def edit():
     print("수정할 줄 번호를 입력해 주세요")
@@ -204,71 +276,9 @@ def edit():
     print("수정 완료:")
     print_line(linenum)
 
-def create_index_cache():
-    if not os.path.isdir("./code"):
-        print("ERROR: code folder was not found")
-        return False
-
-    # subprocess is probably better but whatever ¯\_(ツ)_/¯
-    if not os.path.isdir(WORK_FOLDER):
-        os.mkdir(WORK_FOLDER)
-    print("Creating index cache...")
-    os.system('grep -r ./code/ -e "push\\.cst string" > {}/{}'
-            .format(WORK_FOLDER, INDEX_CACHE_FILE_NAME))
-    print("Index cache was created")
-    return True
-
-def open_index_cache():
-    cache_file_path = WORK_FOLDER + "/" + INDEX_CACHE_FILE_NAME
-    if not os.path.isfile(cache_file_path):
-        return False
-    with open('{}/{}'.format(WORK_FOLDER, INDEX_CACHE_FILE_NAME)) as idx_cache_file:
-        idx_cache_list = idx_cache_file.readlines()
-    print("Index cache was successfully open")
-    global FILE_CONTENTS
-    FILE_CONTENTS = DeserializedLines(idx_cache_list)
-    print("Deserialization complete")
-    return True
-
-class DeserializedLines:
-    class DeserializedLine:
-        def __init__(self, input_line):
-            input_line = input_line.strip()
-            splt_ln = input_line.split(":")
-            self.path = splt_ln[0]
-            self.asmaddr = splt_ln[1]
-
-            idx_quot = input_line.find('"')
-            self.line = input_line[idx_quot+1:-1]
-
-    def __init__(self, input_lines):
-        self.lnlist = []
-        for line in input_lines:
-            self.lnlist.append(self.DeserializedLine(line))
-        self.edited_lines = set()
-
-    def __getitem__(self, key):
-        return self.lnlist[key].line
-
-    def __setitem__(self, key, value):
-        self.lnlist[key].line = value
-
-    def __len__(self):
-        return len(self.lnlist)
-
-
 if __name__ == '__main__':
-    if not open_index_cache():
-        print("Index cache was not found")
-        print("Opening {} instead".format(JSON_FILE_NAME))
-        if not os.path.isfile(JSON_FILE_NAME):
-            print("FATAL: {} was not found too".format(JSON_FILE_NAME))
-            sys.exit(-1)
-        with open(JSON_FILE_NAME) as input_file:
-            # input_file_contents = input_file.read()
-            print("{} was successfully open".format(JSON_FILE_NAME))
-            FILE_CONTENTS = json.load(input_file)
-            print("json successfully parsed")
+    cache_file_path = WORK_FOLDER + "/" + INDEX_CACHE_FILE_NAME
+    INDEX_FILE = StrIndex(cache_file_path)
 
     print_help()
     while 1:
@@ -290,7 +300,7 @@ if __name__ == '__main__':
 
         try:
             for main_prn_ln in read_list:
-                print_line(main_prn_ln)
+                print_line(INDEX_FILE, main_prn_ln)
         except (IndexError, ValueError):
             print("범위 초과")
             continue
